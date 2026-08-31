b985e605558da054e27830c5c279233895b6ad7d
PavelChuu <pavellchu@gmail.com>
2026-08-31 14:27:07 +0300

    Добавлен класс Authentication для проверки пароля Добавлен класс Encryption с методами encryption и decryption. Для шифроки входных данных и их расшифроки

--- src/crypto/encryptor.py
+++ src/crypto/encryptor.py
@@ -1,13 +1,15 @@
 import os
 import base64
+
 from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
 from cryptography.fernet import Fernet
 
-    # Генерация масте-хеша для проверки мастер-пароля
+
+#   Генерация масте-хеша для проверки мастер-пароля
 def generate_master_hash(master_password: str, salt: bytes = None):
     if salt is None:    # Если это первый вход, то сгенерирует salt
         salt = os.urandom(16)
-    
+        
     kdf = Scrypt(
         length = 32,  # Желаемая длинна производного ключа 
         salt = salt,
@@ -15,29 +17,65 @@ def generate_master_hash(master_password: str, salt: bytes = None):
         r=8,          # Размер блока (рекомендуется r=8)
         p=1           # Параллелизм (рекомендуется p=1)
     )
-    
-    master_hash = kdf.derive(master_password.encode())
+    master_hash = kdf.derive(master_password.encode("utf-8"))
     return master_hash, salt
 
- # Шифрование данных
-def get_encryption_key(master_password: str, salt: bytes):
-    kdf = Scrypt(
+
+#   Проверка введёного пароля с master_hash
+class Authentication:
+    def __init__(self, master_hash: str, password: str, salt: bytes):
+        self.master_hash = master_hash
+        self.password = password
+        self.salt = salt
+        
+    def control_master_password(self):
+        kdf = Scrypt(
+                    length = 32,  # Желаемая длинна производного ключа 
+                    salt = self.salt,
+                    n=2**14,      # Стоимость памяти, в большинсттве случаях достаточно 2**14. Для более конфиденциальных данных можно 2**20, но время обработки возрастёт
+                    r=8,          # Размер блока (рекомендуется r=8)
+                    p=1           # Параллелизм (рекомендуется p=1)
+                )
+        kdf.verify(self.password.encode(), self.master_hash)
+        
+
+        
+#   Класс шифрования данных
+class Encryption:
+    # Инициализируем объект
+    def __init__(self, salt: bytes , master_password: str,user_service: str, user_name:str, user_password: str):
+        self.salt = salt
+        self.master_password = master_password
+        self.user_service = user_service
+        self.user_name = user_name
+        self.user_password = user_password
+        
+        #   Создаём ключ для шифрования
+    def get_encryption_key(self):
+        kdf = Scrypt(
             length = 32,  # Желаемая длинна производного ключа 
-            salt = salt,
+            salt = self.salt,
             n = 2**14,      # Стоимость памяти, в большинсттве случаях достаточно 2**14. Для более конфиденциальных данных можно 2**20, но время обработки возрастёт
             r = 8,          # Размер блока (рекомендуется r=8)
             p = 1           # Параллелизм (рекомендуется p=1)
         )
-    
-    return base64.urlsafe_b64encode(kdf.derive(master_password.encode()))
+        key = base64.urlsafe_b64encode(kdf.derive(self.master_password.encode("utf-8")))
+        return key
 
-salt = os.urandom(16)
-password = "78hjy1!"
-
-key = get_encryption_key(password, salt)
-f = Fernet(key)
-
-encrypted_text = f.encrypt("Hello".encode('utf-8'))
-print(encrypted_text)
-decrypted_text = f.decrypt(encrypted_text).decode('utf-8')
-print(decrypted_text)
+    # Шифруем данные
+    def encryption(self):
+        key = self.get_encryption_key()
+        f = Fernet(key)
+        enc_user_service = f.encrypt(self.user_service.encode("utf-8"))
+        enc_user_name = f.encrypt(self.user_name.encode("utf-8"))
+        enc_user_password = f.encrypt(self.user_password.encode("utf-8"))
+        return enc_user_service, enc_user_name, enc_user_password
+    # Дешифровка данных  
+    def decryption(self):
+        key = self.get_encryption_key()
+        f = Fernet(key)
+        
+        dec_user_service = f.decrypt(self.user_service.decode("utf-8"))
+        dec_user_name = f.decrypt(self.user_name.decode("utf-8"))
+        dec_user_password = f.decrypt(self.user_password.decode("utf-8"))
+        return dec_user_service, dec_user_name, dec_user_password
